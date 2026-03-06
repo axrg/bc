f4e2365931254b726230c9ff1e09c03148ce1f5c
Xcaret <axrg00@gmail.com>
2026-03-06 13:34:12 -0600

    update fix

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,12 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "srv1010.hstgr.io",
-  user: "u554104957_assessment2",
-  password: "Y5*lt9Q^j",
-  database: "u554104957_assessment2",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 10
 });
 
 export default pool;
